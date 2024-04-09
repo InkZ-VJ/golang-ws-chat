2734abc5ca671c875fef8600f22da3846d909975
InkZ-VJ <Vatcharapong.j@hotmail.com>
2024-04-09 21:25:12 +0700

    initial state

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+)
+
+func main() {
+       setupAPI()
+
+    log.Fatal(http.ListenAndServe(":3000",nil))
+}
+
+func setupAPI() {
+    http.Handle("/",http.FileServer(http.Dir("./frontend")))
+}
